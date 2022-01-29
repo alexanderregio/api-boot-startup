e3de703caf437915528752e2cfdba8fa80a17ff3
Alexander Régio da Fonseca <alexanderregio@hotmail.com>
2022-01-29 13:04:19 -0300

    Initial

---
+++ ApiBootStartup/ApiBootStartup.cs
@@ -0,0 +1,44 @@
+﻿namespace Libraries;
+
+/// <summary>
+/// Classe que implementa IStartup e pode ser herdada por uma classe Startup na inicialização de uma WebApi
+/// Contém configurações básicas refrequentemente utilizadas por uma API
+/// </summary>
+public abstract class ApiBootStartup : IStartup
+{
+    public IConfiguration Configuration { get; }
+
+    public ApiBootStartup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected abstract void ConfigureWebApiServices(IServiceCollection services);
+    protected abstract void ConfigureWebApi(WebApplication app, IWebHostEnvironment env);
+
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        ConfigureWebApi(app, environment);
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        ConfigureWebApiServices(services);
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
---
+++ ApiBootStartup/StartupExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Libraries;
+
+/// <summary>
+/// Extensão da classe Startup
+/// Objetivo de utilizar classes com a interface IStartup na classe program de uma WebApi
+/// </summary>
+public static class StartupExtensions
+{
+    public static WebApplicationBuilder UseStartup<TStartup>
+        (this WebApplicationBuilder webApplicationBuilder)
+            where TStartup : IStartup
+    {
+        var startup = Activator.CreateInstance
+            (typeof(TStartup), webApplicationBuilder.Configuration)
+                as IStartup;
+
+        if (startup is null)
+            throw new ArgumentNullException(nameof(startup));
+
+        startup.ConfigureServices(webApplicationBuilder.Services);
+
+        var app = webApplicationBuilder.Build();
+
+        startup.Configure(app, app.Environment);
+
+        app.Run();
+
+        return webApplicationBuilder;
+    }
+}
